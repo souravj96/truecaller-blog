abc40504f6bf078e0fced32a59b6e0fb8a8b187c
Sourav Jana <sourav.jana.sourav@gmail.com>
2020-10-01 01:36:50 +0530

    storybook submodule setup

--- src/Screen/LandingPage/index.jsx
+++ src/Screen/LandingPage/index.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 
-class LandingPage extends React.Component{
-    constructor(props){
+// Components
+import { SRow, SCol } from '../../ant_storybook/src/Components/Grid'
+
+class LandingPage extends React.Component {
+    constructor(props) {
         super(props)
     }
 
-    render(){
+    render() {
         return (
-            <p>Welcome</p>
+            <SRow>
+                <SCol span={12}>
+                    <p>Welcome</p>
+                </SCol>
+                <SCol span={12}>
+                    <p>Welcome</p>
+                </SCol>
+            </SRow>
+
         )
     }
 }
